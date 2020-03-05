0bd189dcd38825689d94b6de16a13adfd4e962f6
Irina Gaynanova <irinag@stat.tamu.edu>
2020-03-05 14:50:39 -0600

    Updated comments

--- Code_for_A1C_analysis.R
+++ Code_for_A1C_analysis.R
@@ -2,7 +2,7 @@
 # Creates figures with scores and calculates association with A1C
 ########################################################################################
 # Author: Irina Gaynanova
-# Date of last update: June 10th, 2019
+# Date of last update: February 20th, 2019
 
 # ATTENTION: this code relies on actual A1C values for real CGM data, so can only be run with real data
 
@@ -56,10 +56,11 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
 
 # Specify path to save figures
 figure.path = getwd()
+text_size = 20
 
 
 # Load real CGM data (how the code was used in the paper)
-load("../CGMdataReal.Rdata")
+load("CGMdataReal.Rdata")
 data = CGMdataReal
 minval = minvalReal
 maxval = maxvalReal
@@ -74,14 +75,15 @@ scores_means = out_mfbeta$pca_means$scores
 scores_sds = out_mfbeta$pca_sds$scores
 
 # Combine all the scores and the subject
-dexcom = data.frame(id = subjectIDs, scores_sd1 = scores_sds[,1], scores_sd2 = scores_sds[,2], scores_sd3 = scores_sds[,3], scores1 = scores_means[,1],scores2 = scores_means[,2], scores3 = scores_means[,3])
+# Use -scores for sd 1st component to match orientation for the mean for easer interpretation
+dexcom = data.frame(id = subjectIDs, scores_sd1 = -scores_sds[,1], scores_sd2 = scores_sds[,2], scores_sd3 = scores_sds[,3], scores1 = scores_means[,1],scores2 = scores_means[,2], scores3 = scores_means[,3])
 
 
 ########################################################
 # Load the A1C information (NOT RUN - requires real A1C data)
 ########################################################
 library(readxl)
-newa1c.path = '../Punjabi_Hypnos_2018_01_23.xlsx'
+newa1c.path = '../../CGMS/UpdatedA1C-2018-04-06/Punjabi_Hypnos_2018_01_23.xlsx'
 newa1c = read_excel(newa1c.path, sheet = "results")
 
 
@@ -100,9 +102,9 @@ dexcom_selected <- dexcom[dexcom$id %in% selectedID,]
 dexcom_selected$id <- as.factor(as.character(dexcom_selected$id))
 
 # Scores with color for each subject
-pdf(paste(figure.path, "FirstScoresVsA1c.pdf", sep = ""), onefile = TRUE, width = 16, height = 4)
-p1 = dexcom%>%ggplot(aes(x = scores1, y = a1c)) + geom_point(alpha = 0.3, size = 2) + xlab("Mean score 1") + ylab(expression(paste("HbA"["1c"]," %"))) + geom_point(aes(scores1, a1c, col = id), data = dexcom_selected, size = 3)+ theme(legend.position="none")
-p2 = dexcom%>%ggplot(aes(x = scores_sd1, y = a1c)) + geom_point(alpha = 0.3, size = 2) + xlab("Sd score 1") + ylab(expression(paste("HbA"["1c"]," %"))) + geom_point(aes(scores_sd1, a1c, col = id), data = dexcom_selected, size = 3)+ theme(legend.position="none")
+pdf(paste(figure.path, "/FirstScoresVsA1c.pdf", sep = ""), onefile = TRUE, width = 16, height = 4)
+p1 = dexcom%>%ggplot(aes(x = scores1, y = a1c)) + geom_point(alpha = 0.3, size = 2) + xlab("Mean score 1") + ylab(expression(paste("HbA"["1c"]," %"))) + geom_point(aes(scores1, a1c, col = id), data = dexcom_selected, size = 3)+ theme(legend.position="none")+ theme(text = element_text(size = text_size))
+p2 = dexcom%>%ggplot(aes(x = scores_sd1, y = a1c)) + geom_point(alpha = 0.3, size = 2) + xlab("Sd score 1") + ylab(expression(paste("HbA"["1c"]," %"))) + geom_point(aes(scores_sd1, a1c, col = id), data = dexcom_selected, size = 3)+ theme(legend.position="none")+ theme(text = element_text(size = text_size))
 multiplot(p1,p2, cols = 2)
 dev.off()
 
@@ -110,10 +112,10 @@ dev.off()
 cor(dexcom$a1c, dexcom$scores1, use = "complete.obs") # 0.79
 cor(dexcom$a1c, dexcom$scores_sd1, use = "complete.obs") # 0.60
 
-# Calculate correlations with all other scores and a1c -> all below 0.21
+# Calculate correlations with all other scores and a1c -> all below 0.19
 cor(dexcom$a1c, dexcom$scores2, use = "complete.obs") # -0.19
 cor(dexcom$a1c, dexcom$scores3, use = "complete.obs") # 0.10
-cor(dexcom$a1c, dexcom$scores_sd2, use = "complete.obs") #-0.21
+cor(dexcom$a1c, dexcom$scores_sd2, use = "complete.obs") #-0.18
 cor(dexcom$a1c, dexcom$scores_sd3, use = "complete.obs") # 0.02
 
 
@@ -124,3 +126,7 @@ summary(lm1)$r.squared # 0.64
 # (2) All three scores from mean and sd
 lm2 <- lm(a1c ~ scores1 + scores2+ scores3 + scores_sd1 + scores_sd2 + scores_sd3, data = dexcom)
 summary(lm2)$r.squared # 0.70
+summary(lm2) # first 2 scores significant for mean, first score significant for st.dev.
+
+cor(dexcom$scores1, dexcom$scores_sd1) # 0.63
+cor(dexcom$scores2, dexcom$scores_sd2) # 0.46
